4618da86f5522670a7c6f4bfc09d92b6b4a21608
Alla10475 <sopinskaya10475@gmail.com>
2023-10-29 15:24:45 +0200

    update task-2 and task-4 js

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,19 @@
 function formatMessage(message, maxLength) {
   const messageLength = message.length;
 
-  if (messageLength <= maxLength) {
-    return message;
-  } else {
-    message = message.slice(0, maxLength);
-    return `${message}...`;
-  }
+//   if (messageLength <= maxLength) {
+//     return message;
+//   } else {
+//     message = message.slice(0, maxLength);
+//     return `${message}...`;
+//     }
+    
+    if (messageLength <= maxLength) {
+      return message;
+    } else {
+    //   message = message.slice(0, maxLength);
+      return `${message.slice(0, maxLength)}...`;
+    }
 }
 
 console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
